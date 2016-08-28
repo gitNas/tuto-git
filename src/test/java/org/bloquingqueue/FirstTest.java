ffce23d161525a547307c0e3ffb58ca379d434a9
NassiB <NassiB@ip1.dedicatedip.com>
2016-08-28 22:15:33 +0200

    mvn

---
+++ src/test/java/org/bloquingqueue/FirstTest.java
@@ -0,0 +1,15 @@
+package org.bloquingqueue;
+import org.junit.Assert;
+
+public class FirstTest {
+
+	@org.junit.Test
+	public void testTest() {
+		System.out.println("test");
+	}
+
+	@org.junit.Test
+	public void test2Test() {
+		Assert.assertTrue(false);
+	}
+}
