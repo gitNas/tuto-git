4039d40fd2c0b64cfcb05c54acbbedba6b5f3e57
jenkins <jenkins@sarbacane.com>
2016-08-30 22:25:00 +0200

    master

--- src/test/java/org/bloquingqueue/FirstTest.java
+++ src/test/java/org/bloquingqueue/FirstTest.java
@@ -1,12 +1,8 @@
 package org.bloquingqueue;
+
 import org.junit.Assert;
 
 public class FirstTest {
-
-	@org.junit.Test
-	public void testTest() {
-		System.out.println("test");
-	}
 
 	@org.junit.Test
 	public void test2Test() {
